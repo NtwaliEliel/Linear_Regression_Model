a9326727cd5d24de961e997171f3e19657ce67c1
NtwaliEliel <n.eliel1@alustudent.com>
2025-07-22 00:18:33 +0200

    adding _salary_usd to predicted in api_service

--- summative/FlutterApp/salary_predictor/lib/services/api_service.dart
+++ summative/FlutterApp/salary_predictor/lib/services/api_service.dart
@@ -2,23 +2,26 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static const String apiUrl = "https://linear-regression-model-b50s.onrender.com/predict"; 
+  static const String baseUrl = "https://linear-regression-model-b50s.onrender.com";
 
   static Future<double?> predict(Map<String, dynamic> inputData) async {
     try {
       final response = await http.post(
-        Uri.parse(apiUrl),
+        Uri.parse("$baseUrl/predict"),
         headers: {"Content-Type": "application/json"},
         body: json.encode(inputData),
       );
 
       if (response.statusCode == 200) {
         final body = json.decode(response.body);
-        return body['prediction'] * 1.0;
+        return body['predicted_salary_usd'] * 1.0;
       } else {
+        print('Status code: ${response.statusCode}');
+        print('Response body: ${response.body}');
         return null;
       }
     } catch (e) {
+      print('Error: $e');
       return null;
     }
   }
